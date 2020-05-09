f278662649e9a9eaf9ef376fd5418999d06ff7c9
Matheus Rodrigues <matheusrod92@gmail.com>
2020-05-09 15:14:13 -0300

    :construction: Partially added testimonial carousel

--- components/TestimonialCarousel/TestimonialCarousel.js
+++ components/TestimonialCarousel/TestimonialCarousel.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import Carousel from '@livip/containers/Carousel';
+import { VARIANTS as CAROUSEL_VARIANTS } from '@livip/containers/Carousel/constants';
 
 import Testimonial from '../Testimonial';
 
-const TestimonialCarousel = (props) => {
-  const data = props.testimonials[0];
-  return (
-    <Testimonial {...data} />
-  );
+const TestimonialCarousel = ({ classes, testimonials }) => (
+  <Carousel
+    variant={CAROUSEL_VARIANTS.NAVIGATION_ON_RIGHT}
+    customClasses={classes}
+  >
+    {testimonials.map((testimonial) => <Testimonial {...testimonial} />)}
+  </Carousel>
+);
+
+TestimonialCarousel.propTypes = {
+  testimonials: PropTypes.arrayOf(PropTypes.object),
 };
 
 export default TestimonialCarousel;
--- components/TestimonialCarousel/styles.js
+++ components/TestimonialCarousel/styles.js
@@ -1,3 +1,9 @@
-const styles = (theme) => ({});
+const styles = (theme) => ({
+  root: {
+    '--slider-height-percentage': '20%',
+    // '--content-background-color': 'transparent',
+    'margin-bottom': `${theme.spacing(7)}px`,
+  },
+});
 
 export default styles;
