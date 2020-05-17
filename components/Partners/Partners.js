c974bf4f6ac9f8191d779fda6e6e8de6f81b9b39
Matheus Rodrigues <matheusrod92@gmail.com>
2020-05-17 15:11:32 -0300

    :recycle: Fixed responsivity for partners component

--- components/Partners/Partners.js
+++ components/Partners/Partners.js
@@ -1,26 +1,27 @@
 import React from 'react';
 
+import Box from '@livip/core/Box';
 import Container from '@livip/core/Container';
 import Grid from '@livip/core/Grid';
 
 const Partners = () => (
   <Container>
     <Grid container justify="space-around">
-      <Grid item>
+      <Box component={Grid} item xs={12} sm={6} md={2} textAlign="center">
         <img src="/img/partners-intel.jpg" role="presentation" alt="Parceiro Intel" />
-      </Grid>
-      <Grid item>
+      </Box>
+      <Box component={Grid} item xs={12} sm={6} md={3} textAlign="center">
         <img src="/img/partners-ourofino.jpg" role="presentation" alt="Parceiro Ourofino" />
-      </Grid>
-      <Grid item>
+      </Box>
+      <Box component={Grid} item xs={12} sm={6} md={2} textAlign="center">
         <img src="/img/partners-sanofi.jpg" role="presentation" alt="Parceiro Sanofi" />
-      </Grid>
-      <Grid item>
+      </Box>
+      <Box component={Grid} item xs={12} sm={6} md={3} textAlign="center">
         <img src="/img/partners-proauto.jpg" role="presentation" alt="Parceiro Proauto" />
-      </Grid>
-      <Grid item>
+      </Box>
+      <Box component={Grid} item xs={12} sm={6} md={2} textAlign="center">
         <img src="/img/partners-hp.jpg" role="presentation" alt="Parceiro HP" />
-      </Grid>
+      </Box>
     </Grid>
   </Container>
 );
