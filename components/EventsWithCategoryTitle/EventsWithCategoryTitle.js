58857cead25087fa13cdcd8c6910a94663516569
odelucca <delucca@pm.me>
2020-05-25 17:42:45 -0300

    :bug: Fixing compatibility issues
    To list our projects, we must develop the following page: https://app.zeplin.io/project/5e21f3e014d75a576cbb16f9/screen/5e6e5304b9ae1b17d560de7a
    
    In this commit, I've fixed some compatibility issues with the upstream repository.
    
    Now, our events must be showing as expected.

---
+++ components/EventsWithCategoryTitle/EventsWithCategoryTitle.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Container from '@livipdev/core/Container';
+import Title from '@livipdev/core/Title';
+import EventList from '@livipdev/core/EventList';
+import flow from 'lodash/flow';
+import startCase from 'lodash/startCase';
+import toLower from 'lodash/toLower';
+
+import propTypes from './propTypes';
+import defaultProps from './defaultProps';
+
+const EventsWithCategoryTitle = ({ classes, category, events }) => {
+  const title = {
+    variant: 'h3',
+    message: flow(toLower, startCase)(category),
+  };
+
+  return (
+    <Container>
+      <Title
+        title={title}
+        align="center"
+        customClass={classes.title}
+      />
+      <EventList
+        events={events}
+        customClass={classes.list}
+      />
+    </Container>
+  );
+};
+
+EventsWithCategoryTitle.propTypes = propTypes;
+EventsWithCategoryTitle.defaultProps = defaultProps;
+
+export default EventsWithCategoryTitle;
